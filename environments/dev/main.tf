24931af828d9b97b934a6bf6c183b3bff9e55661
Rahul Wagh <waghrahul60@gmail.com>
2024-05-10 19:01:57 +0530

    added public subnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -14,27 +14,27 @@ module "vpc" {
   }
   project_name            = "my-project"
   environment             = "prod"
-  region                  = "us-west-2"
+  region                  = "us-east-1"
 
   # Subnets variables
   public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
   app_subnet_cidr_blocks    = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones        = ["us-west-2a", "us-west-2b"]
+  availability_zones        = ["us-east-1a", "us-east-1b"]
   map_public_ip_on_launch  = true
 
   # Public Route Table variables
   public_routes = {
     "route1" = {
       cidr_block = "0.0.0.0/0"
-      gateway_id = "igw-1234567890abcdef0"
+      gateway_id = module.vpc.internet_gateway_id
     }
   }
 
-  # Private Route Table variables
-  private_routes = {
-    "route1" = {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "nat-1234567890abcdef0"
-    }
-  }
+  # # Private Route Table variables
+  # private_routes = {
+  #   "route1" = {
+  #     cidr_block = "0.0.0.0/0"
+  #     gateway_id = "nat-1234567890abcdef0"
+  #   }
+  # }
 }
--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  profile = "customprofile"
+  profile = "default"
   region = "us-east-1"
 }
 
